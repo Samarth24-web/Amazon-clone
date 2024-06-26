110e9dad46ff3577e92173929206bac1ef10c782
Samarth Bide <bidesamarth6@gmail.com>
2024-06-26 19:24:03 +0530

    add fuctionality to cheackout.html

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js"; 
+import { cart , removeProductFromCart} from "../data/cart.js"; 
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -15,7 +15,7 @@ cart.forEach((cartItem)=>{
     }
    })
 
-  cartHtml+= `<div class="cart-item-container">
+  cartHtml+= `<div class="cart-item-container js-cart-item-container-${matuchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -38,7 +38,7 @@ cart.forEach((cartItem)=>{
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id=${matuchingProduct.id}>
                     Delete
                   </span>
                 </div>
@@ -94,3 +94,17 @@ cart.forEach((cartItem)=>{
 })
 
 cartSummery.innerHTML=cartHtml;
+
+
+// adding functionality to delete link
+
+const deleteLinks=document.querySelectorAll(".js-delete-link");
+ 
+deleteLinks.forEach((link , i )=>{
+  link.addEventListener("click" , ()=>{
+   const productId=link.dataset.productId;
+   
+   const container=document.querySelector(`.js-cart-item-container-${productId}`);
+   container.remove();
+  })
+})
