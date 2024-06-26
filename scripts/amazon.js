5bb418ae3697126853e2a5716c2ab443b7e8ae08
Samarth Bide <bidesamarth6@gmail.com>
2024-06-26 16:57:57 +0530

    to make a common fixPrice function and fix radio btn problem

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart , addTocartFunctionality} from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 const productList = document.querySelector(".js-product-grid");
 const cartQuantity=document.querySelector(".js-cart-quantity");
@@ -30,7 +31,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            ${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
